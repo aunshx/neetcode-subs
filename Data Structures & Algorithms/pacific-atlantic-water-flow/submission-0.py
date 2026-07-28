fa06d5a981e6d5a82f9c5a15c0c581c0c66f4af8
Aunsh Bandivadekar <86175885+aunshx@users.noreply.github.com>
2026-07-28 09:02:35 -0700

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pac = set()
+        atl = set()
+        neighbours = [(0,1),(1,0),(-1,0),(0,-1)]
+        result = []
+
+        def dfs(r,c,prevHeight,visit):
+            if (min(r,c) < 0 or r == ROWS or c == COLS or (r,c) in visit or prevHeight > heights[r][c]):
+                return 
+            visit.add((r,c))
+            for dr, dc in neighbours:
+                dfs(r + dr, c + dc, heights[r][c], visit)
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                    dfs(0,c,heights[0][c],pac)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                    dfs(r,0,heights[r][0],pac)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                    dfs(ROWS-1,c,heights[ROWS-1][c],atl)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                    dfs(r,COLS-1,heights[r][COLS-1],atl)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r,c) in pac and (r,c) in atl:
+                    result.append([r,c])
+
+        return result
